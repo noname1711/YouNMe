d2779494beae08cd659819f496efbfc950e8927f
DSA <le771485@gmail.com>
2024-07-23 11:03:09 +0700

    update

---
+++ app/src/main/java/com/example/younme/activity/InsertionStatusActivity.kt
@@ -0,0 +1,12 @@
+package com.example.younme.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.younme.R
+
+class InsertionStatusActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_insertion_status)
+    }
+}
